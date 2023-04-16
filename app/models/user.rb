e760fcd0252e8f2c44c55fd7f3123c7164e74416
EC2 Default User <ec2-user@ip-172-31-12-168.ap-northeast-1.compute.internal>
2023-04-16 12:05:04 +0000

    アソシエーション途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
+  has_many :books, dependent: :destroy#:booksってテーブル名？
 
   def get_profile_image(width, height)
     unless profile_image.attached?
