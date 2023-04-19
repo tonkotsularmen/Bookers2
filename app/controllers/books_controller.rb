5ee70b8c3aca73ecbfe4f1c038c161685dad90ea
EC2 Default User <ec2-user@ip-172-31-12-168.ap-northeast-1.compute.internal>
2023-04-19 13:31:25 +0000

    4/19Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,17 +14,32 @@ class BooksController < ApplicationController
 
   def show
     @new = Book.new  #部分テンプレート用
-    @book = Book.find(params[:id])#
+    @book = Book.find(params[:id])#これはほぼ決まった形なので変えない
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 
 end
 
 
-private
-
-def book_params
-  params.require(:book).permit(:title, :body)
-end
