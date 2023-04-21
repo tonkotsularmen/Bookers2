6b948746d712d2fb73d2b49edf411a9d9730e3c5
EC2 Default User <ec2-user@ip-172-31-12-168.ap-northeast-1.compute.internal>
2023-04-21 07:54:20 +0000

    完成か?

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,7 +24,8 @@ class BooksController < ApplicationController
   end
 
   def edit
-    user = User.find(params[:id])
+    book = Book.find(params[:id])
+    user = User.find(book.user.id)
     unless user.id == current_user.id
       redirect_to books_path
     end
