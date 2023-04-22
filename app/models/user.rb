8523b2605d96dcd35fea2ee12c556495b8ce1ac9
EC2 Default User <ec2-user@ip-172-31-12-168.ap-northeast-1.compute.internal>
2023-04-22 05:20:19 +0000

    ERROR 0

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
-  validates :name, length: {minimum: 2 }
+  validates :name, uniqueness: true, length: {minimum: 2, maximum: 20 }
+  validates :introduction, length: {maximum: 50 }
 
   def get_profile_image(width, height)
     unless profile_image.attached?
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:show, :index, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/homes/about" => "homes#about", as: "about"
+  get "/home/about" => "homes#about", as: "about"
 end
