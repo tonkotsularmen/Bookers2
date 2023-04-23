9ecbe14301e9efb95692fa0734a3dd059a12957a
EC2 Default User <ec2-user@ip-172-31-12-168.ap-northeast-1.compute.internal>
2023-04-23 14:03:56 +0000

    コメントの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,16 @@ class ApplicationController < ActionController::Base
   #ただし、topアクションは除くよ
   before_action :configure_permitted_parameters, if: :devise_controller?
   #これの意味
-  def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+  def after_sign_in_path_for(resource)#サインインした後にアクセスするパスは
+    user_path(current_user.id)#（現在ログインしているユーザーのidの）ユーザーパスつまりユーザー詳細画面に行くよ
   end
 
-  def after_sign_out_path_for(resource)
-    root_path
+  def after_sign_out_path_for(resource)#サインアウトした後にアクセスするパスは
+    root_path#ルートパスだよつまりトップ画面にアクセスするよ
   end
 
-  private
+  private#ここから下はこのコントローラでしか呼び出せないよ？
+  
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
