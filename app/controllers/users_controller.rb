1265ff862e978bff4221411e590b5384beffee29
tonkotsularmen <129633387+tonkotsularmen@users.noreply.github.com>
2023-05-19 13:01:42 +0000

    users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def show
     @book = Book.new
     @user = User.find(params[:id])
-    @books = @user.books#これは
-  end
+    @books = @user.books#これで、ユーザーに関連した本の投稿が取得できる。booksとなっているのはmodelにhas_many :booksと書いているから。
+  end                   #@user.booksは複数の投稿が入った配列になっている。
 
   def index
     @book = Book.new
